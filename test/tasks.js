7d5c331ff2679696db618caad960b78360b29b97
George Ornbo <george@shapeshed.com>
2011-12-10 14:56:51 +0000

    WIP for testing

--- test/tasks.js
+++ test/tasks.js
@@ -1,5 +1,12 @@
-describe('api', function(){
-  describe('GET /api/v1/tasks', function(){
-    it('respond with an array of tasks')
+
+var http = require('http');
+var app = require('../app.js');
+
+describe('http', function(){
+  it('should provide an example', function(done){
+    http.get({ path: '/api/v1/tasks', port: 3000 }, function(res){
+      console.log(res.body);
+      done();
+    })
   })
 })
